e1bcf057c5aeb87f74b0a4a36b27ffffb99b7c45
Camille <camille.coutens@gmail.com>
2020-08-19 23:03:19 +0200

    CORS security

--- api/index.js
+++ api/index.js
@@ -6,6 +6,21 @@ const cors = require("cors");
 // Create express instnace
 const app = express()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+
+  // authorized headers for preflight requests
+  // https://developer.mozilla.org/en-US/docs/Glossary/preflight_request
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+
+  app.options('*', (req, res) => {
+      // allowed XHR methods  
+      res.header('Access-Control-Allow-Methods', 'GET, PATCH, PUT, POST, DELETE, OPTIONS');
+      res.send();
+  });
+});
+
 // Init body-parser options (inbuilt with express)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
